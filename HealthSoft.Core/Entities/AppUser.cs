d603116cb7f8b163f8bb5fc2219f713af1d7568f
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-13 00:40:34 +0530

    removed full name from user

--- HealthSoft.Core/Entities/AppUser.cs
+++ HealthSoft.Core/Entities/AppUser.cs
@@ -4,8 +4,6 @@ namespace HealthSoft.Core.Entities
 {
     public class AppUser : IdentityUser
     {
-        public required string FullName { get; set; }
-
         public virtual Doctor? Doctor { get; set; }
         public virtual Patient? Patient { get; set; }
     }
