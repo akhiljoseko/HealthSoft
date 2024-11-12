b513607879fe05d363659508e93f6022ea68cd7d
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-13 00:59:49 +0530

    removed app user fromdb set

--- HealthSoft.Infrastructure/HealthSoftDbContext.cs
+++ HealthSoft.Infrastructure/HealthSoftDbContext.cs
@@ -6,7 +6,6 @@ namespace HealthSoft.Infrastructure
 {
     public class HealthSoftDbContext : IdentityDbContext<AppUser>
     {
-        public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
