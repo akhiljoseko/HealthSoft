b654996398b65bbea2c5f305444c90e8158abf73
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-12 22:07:10 +0530

    Added application db context

---
+++ HealthSoft.Infrastructure/HealthSoftDbContext.cs
@@ -0,0 +1,43 @@
+﻿using HealthSoft.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthSoft.Infrastructure
+{
+    public class HealthSoftDbContext : IdentityDbContext<AppUser>
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        public HealthSoftDbContext(DbContextOptions<HealthSoftDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AppUser>()
+                .HasOne(a => a.Doctor)
+                .WithOne(d => d.AppUser)
+                .HasForeignKey<Doctor>(d => d.AppUserId);
+
+            modelBuilder.Entity<AppUser>()
+                .HasOne(a => a.Patient)
+                .WithOne(p => p.AppUser)
+                .HasForeignKey<Patient>(p => p.AppUserId);
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(d => d.Appointments)
+                .WithOne(a => a.Doctor)
+                .HasForeignKey(a => a.DoctorId);
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Appointments)
+                .WithOne(a => a.Patient)
+                .HasForeignKey(a => a.PatientId);
+        }
+    }
+}
