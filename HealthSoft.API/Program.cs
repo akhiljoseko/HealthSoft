7aa6cf77a7e5583f5b1b219c8117e7f46720e774
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-14 21:43:47 +0530

    Added context and identity service into DI

--- HealthSoft.API/Program.cs
+++ HealthSoft.API/Program.cs
@@ -1,4 +1,14 @@
+using HealthSoft.Core.Entities;
+using HealthSoft.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("HealthSoftDbContextConnection") ?? throw new InvalidOperationException("Connection string 'HealthSoftDbContextConnection' not found.");
+builder.Services.AddDbContext<HealthSoftDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<HealthSoftDbContext>();
 
 // Add services to the container.
 
@@ -8,6 +18,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+// Seeding Admin user
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    await IdentitySeeder.SeedAsync(services);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
