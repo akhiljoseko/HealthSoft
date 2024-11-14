cfa7c20e30473fbe89d62249a3782ebf3960382f
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-14 13:03:35 +0530

    seeding admin user and roles

--- HealthSoft.WebApp/Program.cs
+++ HealthSoft.WebApp/Program.cs
@@ -1,18 +1,34 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using HealthSoft.Infrastructure;
 using HealthSoft.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("HealthSoftDbContextConnection") ?? throw new InvalidOperationException("Connection string 'HealthSoftDbContextConnection' not found.");
 
 builder.Services.AddDbContext<HealthSoftDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<HealthSoftDbContext>();
+builder.Services.AddRazorPages();
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<HealthSoftDbContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Seeding Admin user
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    await IdentitySeeder.SeedAsync(services);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
