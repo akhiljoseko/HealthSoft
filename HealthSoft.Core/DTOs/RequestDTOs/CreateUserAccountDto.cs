54b5090934648fac56523534db2c20eae43ac227
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-14 21:21:52 +0530

    common dto for user account creation

---
+++ HealthSoft.Core/DTOs/RequestDTOs/CreateUserAccountDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthSoft.Core.DTOs.RequestDTOs
+{
+    public class CreateUserAccountDto
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        public required string Role { get; set; }
+        
+        [Required(ErrorMessage = "First name is required")]
+        public required string FirstName { get; set; }
+        public string? LastName { get; set; }
+        
+    }
+}
