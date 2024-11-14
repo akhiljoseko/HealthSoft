182a73d6c846a6e18a7391a287524aea08ff0b71
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-14 22:03:35 +0530

    Added user account controller - Added API to create a user account

---
+++ HealthSoft.API/Controllers/UserAccountController.cs
@@ -0,0 +1,25 @@
+﻿using HealthSoft.Core.DTOs.RequestDTOs;
+using HealthSoft.Core.RepositoryInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthSoft.API.Controllers
+{
+    [Route("api/v1/account")]
+    [ApiController]
+    public class UserAccountController(IUserAccountRepository accountRepository) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateUserAccount(CreateUserAccountDto request)
+        {
+            try
+            {
+                var createdUser = await accountRepository.CreateUserAccountAsync(request);
+                return Ok(createdUser);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ex.Message });
+            }
+        }
+    }
+}
