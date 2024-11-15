bb05efeab54207067668c781db12d3962e549f19
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-15 11:53:43 +0530

    Added add doctor dto class Modified create user dto with common fields

--- HealthSoft.Core/DTOs/RequestDTOs/AddDoctorRequestDto.cs
+++ HealthSoft.Core/DTOs/RequestDTOs/AddDoctorRequestDto.cs
@@ -2,29 +2,8 @@
 
 namespace HealthSoft.Core.DTOs.RequestDTOs
 {
-    public class AddDoctorRequestDto
+    public class AddDoctorRequestDto : CreateUserAccountDto
     {
-        [Required(ErrorMessage = "First name is required")]
-        public required string FirstName { get; set; }
-        public string? LastName { get; set; }
-
-        [Required(ErrorMessage = "Gender is required")]
-        [MaxLength(1, ErrorMessage = "Gender should be a character M/F/O")]
-        [RegularExpression(@"^[MFO]$", ErrorMessage = "Gender must be one of the following: M, F, or O")]
-        public required string Gender { get; set; }
-
-        [Required(ErrorMessage = "Date of Birth is required")]
-        [DataType(DataType.Date)]
-        public DateTime DateOfBirth { get; set; }
-
-        [Required(ErrorMessage = "Contact number is required")]
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid contact number")]
-        public required string ContactNumber { get; set; }
-
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email address")]
-        public required string Email { get; set; }
-
         [Required(ErrorMessage = "Specialization is required")]
         public required string Specialization { get; set; }
 
@@ -34,9 +13,8 @@ namespace HealthSoft.Core.DTOs.RequestDTOs
         [Required(ErrorMessage = "Employement start date is required")]
         [DataType(DataType.Date)]
         public required DateTime EmploymentStartDate { get; set; }
-        public DateTime? EmploymentEndDate { get; set; }
 
-        [Required(ErrorMessage = "AppUserId is required")]
-        public required string AppUserId { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EmploymentEndDate { get; set; }
     }
 }
--- HealthSoft.Core/DTOs/RequestDTOs/CreateUserAccountDto.cs
+++ HealthSoft.Core/DTOs/RequestDTOs/CreateUserAccountDto.cs
@@ -2,9 +2,11 @@
 
 namespace HealthSoft.Core.DTOs.RequestDTOs
 {
-    public class CreateUserAccountDto
+    public abstract class CreateUserAccountDto
     {
+
         [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email address")]
         public required string Email { get; set; }
 
         [Required(ErrorMessage ="Password is required")]
@@ -13,10 +15,23 @@ namespace HealthSoft.Core.DTOs.RequestDTOs
 
         [Required(ErrorMessage = "Role is required")]
         public required string Role { get; set; }
-        
+
         [Required(ErrorMessage = "First name is required")]
         public required string FirstName { get; set; }
         public string? LastName { get; set; }
-        
+
+        [Required(ErrorMessage = "Gender is required")]
+        [MaxLength(1, ErrorMessage = "Gender should be a character M/F/O")]
+        [RegularExpression(@"^[MFO]$", ErrorMessage = "Gender must be one of the following: M, F, or O")]
+        public required string Gender { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid contact number")]
+        public required string ContactNumber { get; set; }
+
     }
 }
