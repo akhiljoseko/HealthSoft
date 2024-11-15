c1fea0ed0beabada8bb1d3373a6970770b57b063
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-15 17:21:59 +0530

    Created login request and response dtos

---
+++ HealthSoft.Core/DTOs/RequestDTOs/LoginRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LoginRequestDto
+{
+    [Required(ErrorMessage = "Email is required")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    public  required string Password { get; set; }
+}
---
+++ HealthSoft.Core/DTOs/ResponseDTOs/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthSoft.Core.DTOs.ResponseDTOs
+{
+    public class LoginResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? Email { get; set; }
+        public string? AppUserId { get; set; }
+        public string? Token { get; set; }
+    }
+}
