8784917eef18c03e81dab5946881cfcb05d6a953
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-15 16:12:39 +0530

    Interface and implementation of appointment repository

---
+++ HealthSoft.Core/RepositoryInterfaces/IAppointmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using HealthSoft.Core.DTOs.RequestDTOs;
+using HealthSoft.Core.Entities;
+
+namespace HealthSoft.Core.RepositoryInterfaces
+{
+    public interface IAppointmentRepository
+    {
+        Task<Appointment> BookAppointment(BookAppointmentRequestDto requestDto);
+        Task<bool> CancelAppointment(int appointmentId);
+        Task<IEnumerable<Appointment>> GetAllAppointments();
+        Task<IEnumerable<Appointment>> GetAppointmentsByDoctorId(int doctorId);
+    }
+}
---
+++ HealthSoft.Infrastructure/Repositories/AppointmentRepository.cs
@@ -0,0 +1,57 @@
+﻿using HealthSoft.Core.DTOs.RequestDTOs;
+using HealthSoft.Core.Entities;
+using HealthSoft.Core.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthSoft.Infrastructure.Repositories
+{
+    public class AppointmentRepository(HealthSoftDbContext context) : IAppointmentRepository
+    {
+        public async Task<Appointment> BookAppointment(BookAppointmentRequestDto requestDto)
+        {
+            var appointment = new Appointment
+            {
+                DoctorId = requestDto.DoctorId,
+                PatientId = requestDto.PatientId,
+                ReasonForVisit = requestDto.Purpose,
+                AppointmentDateTime = requestDto.AppointmentDateTime,
+                Status = "Booked" 
+            };
+
+            await context.Appointments.AddAsync(appointment);
+            await context.SaveChangesAsync();
+
+            return appointment;
+        }
+
+        public async Task<bool> CancelAppointment(int appointmentId)
+        {
+            var appointment = await context.Appointments.FindAsync(appointmentId);
+            if (appointment == null)
+                return false;
+
+            appointment.Status = "Cancelled";
+            context.Appointments.Update(appointment);
+            await context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<IEnumerable<Appointment>> GetAllAppointments()
+        {
+            return await context.Appointments
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Appointment>> GetAppointmentsByDoctorId(int doctorId)
+        {
+            return await context.Appointments
+                .Where(a => a.DoctorId == doctorId)
+                .Include(a => a.Doctor)
+                .Include(a => a.Patient)
+                .ToListAsync();
+        }
+    }
+}
