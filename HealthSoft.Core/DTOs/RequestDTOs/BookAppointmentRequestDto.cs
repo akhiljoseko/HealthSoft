78d45114fc3a2476caf35d9f6f37ff9e872d48d8
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-15 16:13:03 +0530

    request dto for appointment booking

---
+++ HealthSoft.Core/DTOs/RequestDTOs/BookAppointmentRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthSoft.Core.DTOs.RequestDTOs
+{
+    public class BookAppointmentRequestDto
+    {
+        [Required(ErrorMessage = "Patient ID is required.")]
+        public required int PatientId { get; set; }
+
+        // Doctor Details
+        [Required(ErrorMessage = "Doctor ID is required.")]
+        public required int DoctorId { get; set; }
+
+        // Appointment Details
+        [Required(ErrorMessage = "Appointment date and time is required.")]
+        [DataType(DataType.DateTime)]
+        public required DateTime AppointmentDateTime { get; set; }
+        public required string Purpose { get; set; }
+
+    }
+}
