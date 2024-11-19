86737801f8dc8d5806a92b4c88ebb9cede55a2e4
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-19 18:19:39 +0530

    authentication controller added

---
+++ HealthSoft.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using HealthSoft.Core.RepositoryInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthSoft.API.Controllers
+{
+    [Route("api/V1/auth")]
+    [ApiController]
+    public class AuthenticationController(IAuthenticationRepository authenticationRepository) : ControllerBase
+    {
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            try
+            {
+                var loginResponse = await authenticationRepository.PasswordLogin(request);
+                if (!loginResponse.IsSuccess)
+                {
+                    return Unauthorized(loginResponse.ErrorMessage);
+                }
+                return Ok(loginResponse);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ex.Message });
+            }
+        }
+    }
+}
