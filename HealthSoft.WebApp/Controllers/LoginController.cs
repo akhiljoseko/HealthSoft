39e7b3ac647bd8f7aa65655c699bc8ac61c4ef7c
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-20 22:05:11 +0530

    Created login controller  - Added index method  - Added login method

---
+++ HealthSoft.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using HealthSoft.Core.RepositoryInterfaces;
+using HealthSoft.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthSoft.WebApp.Controllers
+{
+    public class LoginController(IAuthenticationRepository authenticationRepository) : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index (LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var loginRequest = new LoginRequestDto
+                {
+                    Email = loginModel.EmailId,
+                    Password = loginModel.Password,
+                };
+                var result = await authenticationRepository.PasswordLogin(loginRequest);
+                if (result.IsSuccess)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Invalid login credentials. Please try again";
+                    return View(loginModel);
+                }
+            }
+            return View(loginModel);
+        }
+    }
+}
