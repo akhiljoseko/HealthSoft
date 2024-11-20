bd8f23e9993723a719d61db1f24bd82c88f4fb96
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-20 16:58:20 +0530

    Created new web app

--- HealthSoft.WebApp/Program.cs
+++ HealthSoft.WebApp/Program.cs
@@ -1,34 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using HealthSoft.Infrastructure;
-using HealthSoft.Core.Entities;
-using Microsoft.AspNetCore.Identity;
-
 var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("HealthSoftDbContextConnection") ?? throw new InvalidOperationException("Connection string 'HealthSoftDbContextConnection' not found.");
-
-builder.Services.AddDbContext<HealthSoftDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddRazorPages();
-builder.Services.AddIdentity<AppUser, IdentityRole>()
-    .AddEntityFrameworkStores<HealthSoftDbContext>()
-    .AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Seeding Admin user
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    await IdentitySeeder.SeedAsync(services);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -48,7 +23,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
 
 app.Run();
