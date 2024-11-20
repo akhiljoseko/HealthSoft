f92b1fba6926b2a9263fb4b1de7f22c65ddb0a86
akhiljoseko <dev.akhiljoseko@gmail.com>
2024-11-20 22:05:22 +0530

    Created login view model class

---
+++ HealthSoft.WebApp/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthSoft.WebApp.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public required string EmailId { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public required string Password { get; set; }
+    }
+}
